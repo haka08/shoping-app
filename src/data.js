b5742b8521fa80e921582dbf2ecb187d07686f36
haka08 <hafsakabeer008@gmail.com>
2022-04-06 00:50:50 -0400

    first commit

---
+++ src/data.js
@@ -0,0 +1,64 @@
+const products = [
+  {
+    _id: "1",
+    name: "Shoes wow",
+    image:
+      "https://images.unsplash.com/photo-1600185365483-26d7a4cc7519?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=725&q=80",
+
+    price: 89.99,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: "2",
+    name: "Shoes again",
+    image:
+      "https://images.unsplash.com/photo-1602033693387-3531914e7185?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1029&q=80",
+
+    price: 599.99,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: "3",
+    name: "More shoes",
+    image:
+      "https://images.unsplash.com/photo-1600185365778-7875a359b924?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=725&q=80",
+
+    price: 929.99,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: "4",
+    name: "Omg???? shoes???",
+    image:
+      "https://images.unsplash.com/photo-1622760807800-66cf1466fc08?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=465&q=80",
+
+    price: 399.99,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: "5",
+    name: "don't tell me. shoes.",
+    image:
+      "https://images.unsplash.com/photo-1572969565371-acc5b2b3496a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+
+    price: 49.99,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: "6",
+    name: "shoes? more like shwhoasked",
+    image:
+      "https://images.unsplash.com/photo-1552346154-21d32810aba3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+
+    price: 29.99,
+    rating: 4,
+    numReviews: 12,
+  },
+];
+
+export default products;
